a480a10a6e9943f3fde8031de83ab84006c882bf
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2019-07-24 17:07:41 +0200

    refactored code to split prediction part from scores of the model

--- nbs/evaluate_model.py
+++ nbs/evaluate_model.py
@@ -1,14 +1,11 @@
-
 import os
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-# from helpers import data_loaders as dls
-# from helpers.viz import plot, plot_history
-# from helpers.logger import Logger
 import utils
 import dl_models
+from helpers.timer import timer
 import keras # this is required
 from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
@@ -18,6 +15,7 @@ from keras.optimizers import *
 def load_dataset(add_geometrical_features=True,
                   subsample_flag=True,
                   compute_HOG=False):
+
     PATH = '../'  # path of the repo.
     _NAME = 'experiment0'  # name of experiment
 
@@ -26,18 +24,10 @@ def load_dataset(add_geometrical_features=True,
                                      add_geometrical_features=add_geometrical_features,
                                      subsample=subsample_flag,
                                      compute_HOG=compute_HOG)
-    # number of channels in the images
-    n_channels = 6
-    if add_geometrical_features:
-        n_channels += 3
-    if compute_HOG:
-        n_channels += 6
 
     f_train, f_valid, f_test, gt_train, gt_valid, gt_test = KPC.get_dataset(limit_index = -1)
 
-    return f_test, gt_test, n_channels
-
-
+    return f_test, gt_test
 
 
 def get_info_from_test_name(filename):
@@ -71,6 +61,11 @@ def initialize_model(model_name, weights, training_config, shape):
         model = dl_models.get_lodnn_model(shape=shape)
     elif model_name == 'unet':
         model = dl_models.get_unet_model(input_size=shape)
+    elif model_name == 'unet6':
+        model = dl_models.u_net6(shape=shape,
+                                 filters=512,
+                                 int_space=32,
+                                 output_channels=2)
     else:
         raise ValueError("model_name nor lodnn neither unet")
 
@@ -88,6 +83,44 @@ def initialize_model(model_name, weights, training_config, shape):
 
     return model
 
+@timer
+def predict_sample(model, sample):
+    prediction = model.predict(sample)
+    return prediction[0, :, :, :]
+
+def predict_test_set(model, f_test):
+    all_pred = []
+
+    for f in f_test:
+        prediction = predict_sample(model, np.expand_dims(f, axis=0))
+        all_pred.append(prediction)
+
+    all_pred = np.array(all_pred)
+
+    return all_pred
+
+def compute_scores(pred, gt):
+
+    all_f1, all_recall, all_prec, all_acc = [], [], [], []
+
+    for n in range(len(pred)):
+
+        f1, recall, precision, acc = utils.get_metrics(gt=gt[n, :, :, 0], pred=utils.apply_argmax(pred[n, :, :, :]))
+        print("Scores for sample {}: F1 -> {}, Recall -> {}, Precision -> {}, Accuracy -> {}".format(n, f1, recall, precision, acc))
+        all_f1.append(f1)
+        all_recall.append(recall)
+        all_prec.append(precision)
+        all_acc.append(precision)
+
+    print('------------------------------------------------')
+
+    scores = {'f1': all_f1,
+              'recall': all_recall,
+              'precision': all_prec,
+              'acc': all_acc}
+
+    return scores
+
 def evaluate_model(model_name, weights):
     # getting dir
     weights_dir = os.path.abspath(os.path.dirname(weights))
@@ -99,34 +132,44 @@ def evaluate_model(model_name, weights):
     geometric, hog, sampled, training_config = get_info_from_test_name(os.path.join(weights_par_dir, 'details.json'))
     print(training_config)
 
-    f_test, gt_test, n_channels = load_dataset(add_geometrical_features=geometric,
+    f_test, gt_test = load_dataset(add_geometrical_features=geometric,
                                    subsample_flag=sampled,
                                    compute_HOG=hog)
 
-    # Test
-    test_datagen = ImageDataGenerator() #horizontal_flip=True #add this ?
-    test_iterator = test_datagen.flow(f_test, gt_test,
-                    batch_size=1, shuffle=True)
 
-    model = initialize_model(model_name, weights, training_config, (400, 200, n_channels))
+    # image shape
+    n_row, n_col, n_channels = f_test.shape[1:]
 
-    all_pred = model.predict_generator(test_iterator,
-                                       steps=f_test.shape[0])
-    all_pred = np.array(all_pred)
+    if 'unet' in model_name:
+        multiple_of = 16 if model_name =='unet' else 32
+        nrpad =  multiple_of - n_row % multiple_of if n_row % multiple_of != 0 else 0
+        ncpad =  multiple_of - n_col % multiple_of if n_col % multiple_of != 0 else 0
 
-    for n in range(len(all_pred)):
-        f, ax = plt.subplots(1,4)
-        ax[0].imshow(gt_test[n, :,:, 0])
+        f_test = np.pad(f_test, ((0, 0), (0, nrpad), (0, ncpad), (0, 0)), 'constant')
+
+    # initializing model
+    model = initialize_model(model_name, weights, training_config, (n_row, n_col, n_channels))
+
+    pred = predict_test_set(model, f_test)
+
+    if 'unet' in model_name:
+        scores = compute_scores(pred[:, :n_row, :n_col, :], gt_test)
+    else:
+        scores = compute_scores(pred, gt_test)
+
+    for n in range(len(pred)):
+        f, ax = plt.subplots(1, 4)
+        ax[0].imshow(gt_test[n, :, :, 0])
         ax[0].set_title('gt_{}'.format(n))
-        ax[1].imshow(all_pred[n, :, :, 0])
+        ax[1].imshow(pred[n, :, :, 0])
         ax[1].set_title('pred_0_{}'.format(n))
-        ax[2].imshow(all_pred[n, :, :, 1])
+        ax[2].imshow(pred[n, :, :, 1])
         ax[2].set_title('pred_1_{}'.format(n))
-        ax[3].imshow(1 - np.argmax(all_pred[n, :, :, :], axis=2))
+        ax[3].imshow(1 - np.argmax(pred[n, :, :, :], axis=2))
         ax[3].set_title('pred_argmax_{}'.format(n))
         plt.show()
 
-    return all_pred
+    return scores
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Road Segmentation")
