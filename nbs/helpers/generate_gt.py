50c6b01b6890110091b7b73186ce50da094833c2
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2019-08-29 16:15:30 +0200

    removed information coming from bev_gt; now gt for point cloud is generated only looking to camera image

--- nbs/helpers/generate_gt.py
+++ nbs/helpers/generate_gt.py
@@ -1,13 +1,28 @@
 import os
 import numpy as np
 from .calibration import Calibration
-from .data_loaders import load_img, load_bin_file, get_image, get_dataset
+from .data_loaders import load_bin_file, get_image, get_dataset
 from .calibration import get_lidar_in_image_fov
 
+
 def project_points_on_camera_img(points, img, calib):
-    '''
+    """
     Function that  project point cloud on fron view image and retrieve RGB information
-    '''
+
+    Parameters
+    ----------
+    points: ndarray
+        point cloud
+    img: ndarray
+        camera image
+    calib:  Calibration
+        calibration object
+
+    Returns
+    -------
+    points_to_pxls: ndarray
+        table that associate to each point in point cloud the corresponding pixel in camera image
+    """
     height, width = img.shape[:2]
 
     imgfov_pc_velo, pts_2d, fov_inds = get_lidar_in_image_fov(points, calib, 0, 0, width, height,
@@ -24,6 +39,19 @@ def project_points_on_camera_img(points, img, calib):
 
 
 def project_on_bev_img(points):
+    """
+    Function that project point cloud to bev image
+
+    Parameters
+    ----------
+    points: ndarray
+        input point cloud
+
+    Returns
+    -------
+    points_to_bev_pxls: ndarray
+        table representing for each point the corresponding pixel in the bev image
+    """
     side_range = (-10, 10)
     fwd_range = (6, 46)
     res = .1
@@ -45,34 +73,76 @@ def project_on_bev_img(points):
     return points_to_bev_pxls
 
 
-def in_bev_img(pxl, shape):
+def in_img_frame(pxl, shape):
+    """
+    Function that check if pixel is in the image frame
+
+    Parameters
+    ----------
+    pxl: tuple
+        pixels coordinates
+
+    shape: tuple
+        image shape
+
+    Returns
+    -------
+
+    check: bool
+        True if pxl is in image frame. False otherwise
+
+    """
     i, j = pxl
     nr, nc = shape[:2]
-    if i >= 0 and i < nr and j >= 0 and j < nc:
+    if (i >= 0) and (i < nr) and (j >= 0) and (j < nc):
         return True
     else:
         return False
 
-def generate_point_cloud_gt(points, gt_img, gt_bev, calib):
-    points_to_cam_pxl = project_points_on_camera_img(points[:,:3], gt_img, calib)
-    points_to_bev_pxl = project_on_bev_img(points[:,:3])
+
+def generate_point_cloud_gt(points, gt_img, calib):
+    """
+    Function used to project gt from camera image to point cloud
+
+    Parameters
+    ----------
+    points: ndarray
+        input point cloud
+
+    gt_img: ndarray
+        Ground truth camera image
+
+    calib: Calibration
+        Calbration object used to map point cloud to RGB image
+
+    Returns
+    -------
+    points: ndarray
+        point cloud with gt
+
+    """
+    points_to_cam_pxl = project_points_on_camera_img(points[:, :3], gt_img, calib)
     cam_labels = np.zeros(len(points), dtype=np.uint8)
     bev_labels = np.zeros(len(points), dtype=np.uint8)
-
-    bev_shape = (400, 200)
 
     for n in range(len(points)):
         if points_to_cam_pxl[n, 0] > 0:
             cam_labels[n] = max(gt_img[points_to_cam_pxl[n, 0], points_to_cam_pxl[n, 1], 0] // 255, cam_labels[n])
 
-        # if in_bev_img(points_to_bev_pxl[n], bev_shape) and (points[n,2] > -3):
-        #         bev_labels[n] = max(gt_bev[points_to_bev_pxl[n, 0], points_to_bev_pxl[n, 1], 0] // 255, bev_labels[n])
-
     labels = np.logical_or(cam_labels, bev_labels)
 
     return np.c_[points, labels]
 
+
 def generate_gt(path):
+    """
+    Function that loops over all the database and add gt to point clouds
+
+    Parameters
+    ----------
+    path: str
+        path to database root
+    """
     train_set, valid_set, test_set = get_dataset(path, is_training=True)
 
     # joining all the lists together
@@ -94,16 +164,15 @@ def generate_gt(path):
                                                              calib_fileslist):
         # loading data from file
         pc = load_bin_file(pc_path)
-        gt_bev = get_image(gt_bev_path, is_color=True, rgb=False)
         gt_img = get_image(gt_img_path, is_color=True, rgb=False)
         calib = Calibration(calib_path)
         # retrieving labels for point clouds
-        points = generate_point_cloud_gt(pc, gt_img, gt_bev, calib)
+        points = generate_point_cloud_gt(pc, gt_img, calib)
         filename = pc_path.split('/')[-1]
         print(os.path.join(pc_new_dir, filename))
         points.astype(np.float32).tofile(os.path.join(pc_new_dir, filename))
 
+
 if __name__ == "__main__":
     PATH = '../../'
-
     generate_gt(os.path.abspath(PATH))
